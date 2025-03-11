12ebb1855e51779a85fb4bd397ef161b35d74759
saem020402 <sem020402@g.skku.edu>
2025-03-11 19:40:57 +0900

    feat: 채팅메세지 색상 수정

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -7,7 +7,7 @@ const Message = ({ sender, message }) => {
         <div className={`flex ${isUser ? 'justify-end' : 'justify-start'} mt-4 mb-10`}>
             <div
                 className={`max-w-[80%] p-4 rounded-lg text-xl leading-relaxed break-keep whitespace-pre-wrap
-                    ${isUser ? 'bg-yellow-400 text-white rounded-br-none' : 'bg-[#1B3764] text-white rounded-bl-none'}
+                    ${isUser ? 'bg-yellow-400 text-white rounded-br-none' : 'bg-gray-100 text-gray-900 rounded-bl-none'}
                 `}
             >
                 {/* bg-gray-100 text-gray-900 bg-[#1B3764] text-white */}
