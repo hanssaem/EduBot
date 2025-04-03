317766af94f5ff472e70588e82caabf13c428c10
saem020402 <sem020402@g.skku.edu>
2025-04-03 18:45:13 +0900

    feature: modify note API, add folder API

--- backend/models/Note.js
+++ backend/models/Note.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const noteSchema = new mongoose.Schema({
     userId: { type: String, required: true }, // User email
     title: { type: String, required: true },
+    folderId: { type: mongoose.Schema.Types.ObjectId, ref: "Folder", default: null },
     content: { type: String, required: true },
     createdAt: { type: Date, default: Date.now }
 });
