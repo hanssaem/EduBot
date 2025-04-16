a4062324df2efc4d2c0a60390028f4bf44cf95f5
seungju lee <dltmdwn0322@gmail.com>
2025-04-17 03:23:51 +0900

    chore: Modal 컴포넌트 prop 및 동작 수정

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -1,33 +1,39 @@
 export default function Modal({
   isOpen,
-  setIsOpen,
   title,
   content,
   useCancelButton = true,
   useConfirmButton = true,
   confirmText = '확인',
+  mode = 'default',
+  onClose = () => {},
   onConfirm = () => {},
 }) {
+  const confirmButtonClass =
+    mode === 'error'
+      ? 'btn bg-red-500 hover:bg-red-600 text-white font-pretendard rounded-2xl'
+      : 'btn bg-[#1B3764] text-white font-pretendard rounded-2xl';
+
   return (
     <dialog className={`modal ${isOpen ? 'open' : ''}`} open={isOpen}>
-      <div className="modal-box">
+      <div className="modal-box rounded-2xl">
         <h3 className="font-bold text-lg font-pretendard">{title}</h3>
         <div className="py-4 font-pretendard">{content}</div>
         <div className="modal-action">
           {useCancelButton && (
             <button
-              className="btn font-pretendard"
-              onClick={() => setIsOpen(false)}
+              className="btn font-pretendard rounded-2xl"
+              onClick={onClose}
             >
               닫기
             </button>
           )}
           {useConfirmButton && (
             <button
-              className="btn bg-[#1B3764] text-white font-pretendard"
+              className={confirmButtonClass}
               onClick={() => {
                 onConfirm();
-                setIsOpen(false);
+                onClose();
               }}
             >
               {confirmText}
